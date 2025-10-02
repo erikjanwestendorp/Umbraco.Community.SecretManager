ac2e71acb077212295ff57599c78aa912cd82f2f
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2025-10-02 20:32:18 +0200

    Update Options

--- src/Umbraco.Community.SecretManager/Configuration/SecretManagerOptions.cs
+++ src/Umbraco.Community.SecretManager/Configuration/SecretManagerOptions.cs
@@ -1,10 +1,7 @@
-﻿using Azure.Security.KeyVault.Secrets;
-
-namespace Umbraco.Community.SecretManager.Configuration;
+﻿namespace Umbraco.Community.SecretManager.Configuration;
 
 public class SecretManagerOptions
 {
-    public required SecretClient SecretClient { get; set; }
     public bool EnableUmbracoUiBuilder { get; set; } = false;
     public TimeSpan Period { get; set; } = TimeSpan.FromDays(1);
     public string? FirstRun { get; set; }
